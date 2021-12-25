b1f870edae4071a28b2e3da41f4a053dea26d3b5
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-12-25 14:35:33 +0500

    app.js file added

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".form");
+const getInputNum = document.querySelector(".get-number");
+const resultNum = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (getInputNum.value % 3 === 0 && getInputNum.value % 5 === 0) {
+    resultNum.textContent = "FizzBuzz";
+  } else if (getInputNum.value % 3 === 0) {
+    resultNum.textContent = "Fizz";
+  } else if (getInputNum.value % 5 === 0) {
+    resultNum.textContent = "Buzz";
+  } else {
+    resultNum.textContent = getInputNum.value;
+  }
+});
