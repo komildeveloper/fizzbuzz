7db234c1a448078b74754c9c736504cfd8a18f0a
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-02-01 11:47:56 +0500

    update app.js file && formatted

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const form = document.querySelector(".form");
-const getInputNum = document.querySelector(".get-number");
-const resultNum = document.querySelector(".result");
+const form = document.querySelector('.form');
+const getInputNum = document.querySelector('.get-number');
+const resultNum = document.querySelector('.result');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 
   if (getInputNum.value % 3 === 0 && getInputNum.value % 5 === 0) {
-    resultNum.textContent = "FizzBuzz";
+    resultNum.textContent = 'FizzBuzz';
   } else if (getInputNum.value % 3 === 0) {
-    resultNum.textContent = "Fizz";
+    resultNum.textContent = 'Fizz';
   } else if (getInputNum.value % 5 === 0) {
-    resultNum.textContent = "Buzz";
+    resultNum.textContent = 'Buzz';
   } else {
     resultNum.textContent = getInputNum.value;
   }
